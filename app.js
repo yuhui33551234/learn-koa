be8334ee2a32089d62010542ddc8ccec7475c585
weijia <weijiapuhui@163.com>
2019-06-27 22:44:53 +0800

    lll

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const Koa = require('koa')
 const app = new Koa();
+
+app.use(async (ctx, next)=>{
+    console.log(`${ctx.request.method} ${ctx.request.url}`)
+    await next()
+})
+
+app.use(async (ctx, next)=>{
+    const start = new Date().getTime();
+    await next()
+    const ms = new Date().getTime()-start;
+    console.log(`Time :${ms}ms`)
+})
+
 app.use(async(ctx, next)=>{
     await next();
     ctx.response.type = 'text/html';
